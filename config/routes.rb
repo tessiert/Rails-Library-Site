02010cf93ad0c740bbe2974f0c97a92457a9b6cd
Trace Tessier <tessiert@unm.edu>
2018-12-18 16:24:32 -0700

    Initial Commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'book/list'
+  get 'book/new'
+  post 'book/create'
+  patch 'book/update'
+  get 'book/show'
+  get 'book/edit'
+  get 'book/delete'
+  get 'book/show_books_by_subject'
+end
